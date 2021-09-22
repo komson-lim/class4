9c9be694f95a055638322cc1c9448dcb1b4e0d2a
komson <shootingstar7447@gmail.com>
2021-09-22 21:35:51 +0700

    Fix query post_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -60,7 +60,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = User.find(params[:user_id]).posts.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
